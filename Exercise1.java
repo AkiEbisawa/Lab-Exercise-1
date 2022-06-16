980856bd47e81fee6c19c0a3c55193bdfebe2681
Aki Ebisawae <akihthak0708pk@gmail.com>
2022-06-15 23:51:37 -0700

    Sorry for the late hand in

---
+++ Exercise1.java
@@ -0,0 +1,13 @@
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		
+		int r = 3;
+		System.out.println("The radius is:" + r);
+		System.out.println("The area is:" + r * r * Math.PI);
+		
+		
+	}
+
+}
---
+++ Exercise2.java
@@ -0,0 +1,29 @@
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		
+		int cents = 324;
+		int dollars = cents / 100;
+		
+	System.out.println("The values is " + dollars + " and " + cents);
+	
+	}
+
+}
+
+//import java.util.Scanner;
+//
+//public class Exercise2 {
+//
+// public static void main(String[] args) {
+//
+//  Scanner scan = new Scanner(System.in);
+//  System.out.println("Input the cents:");
+//  int cents = scan.nextInt();
+//  System.out.printf("The value is %d dollars and %d cents", (cents/100), (cents%100));
+//  scan.close();
+//
+// }
+//
+//}
+// I could not find the way to print cents.
